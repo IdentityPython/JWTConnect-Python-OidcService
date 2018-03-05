5a24f43dd1ec29824f91e18936b4396be4a68c2c
roland <roland@catalogix.se>
2018-03-05 08:24:30 +0100

    upped the version too

--- src/oidcservice/__init__.py
+++ src/oidcservice/__init__.py
@@ -8,7 +8,7 @@ except ImportError:
     import random as rnd
 
 __author__ = 'Roland Hedberg'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 OIDCONF_PATTERN = "{}/.well-known/openid-configuration"
