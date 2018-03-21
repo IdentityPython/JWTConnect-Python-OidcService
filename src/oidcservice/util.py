f086eb844c8686ac3b342bee746d1889de1b4060
roland <roland@catalogix.se>
2018-03-21 10:06:00 +0100

    Spring cleaning

--- src/oidcservice/util.py
+++ src/oidcservice/util.py
@@ -12,38 +12,21 @@ __author__ = 'roland'
 URL_ENCODED = 'application/x-www-form-urlencoded'
 JSON_ENCODED = "application/json"
 
-DEFAULT_POST_CONTENT_TYPE = URL_ENCODED
 
-PAIRS = {
-    "port": "port_specified",
-    "domain": "domain_specified",
-    "path": "path_specified"
-}
+def get_http_url(url, req, method='GET'):
+    """
+    Add a query part representing the request to a url that may already contain
+    a query part. Only done if the HTTP method used is 'GET' or 'DELETE'.
 
-ATTRS = {"version": None,
-         "name": "",
-         "value": None,
-         "port": None,
-         "port_specified": False,
-         "domain": "",
-         "domain_specified": False,
-         "domain_initial_dot": False,
-         "path": "",
-         "path_specified": False,
-         "secure": False,
-         "expires": None,
-         "discard": True,
-         "comment": None,
-         "comment_url": None,
-         "rest": "",
-         "rfc2109": True}
-
-
-def get_http_url(uri, req, method='GET'):
+    :param url: The URL
+    :param req: The request as a :py:class:`oidcmsg.message.Message` instance
+    :param method: The HTTP method
+    :return: A possibly modified URL
+    """
     if method in ["GET", "DELETE"]:
         if req.keys():
             _req = req.copy()
-            comp = urlsplit(str(uri))
+            comp = urlsplit(str(url))
             if comp.query:
                 _req.update(parse_qs(comp.query))
 
@@ -51,12 +34,21 @@ def get_http_url(uri, req, method='GET'):
             return urlunsplit((comp.scheme, comp.netloc, comp.path,
                                _query, comp.fragment))
         else:
-            return uri
+            return url
     else:
-        return uri
+        return url
 
 
-def get_http_body(req, content_type):
+def get_http_body(req, content_type=URL_ENCODED):
+    """
+    Get the message into the format that should be places in the body part
+    of a HTTP request.
+
+    :param req: The service request as a  :py:class:`oidcmsg.message.Message`
+        instance
+    :param content_type: The format of the body part.
+    :return: The correctly formatet service request.
+    """
     if content_type == URL_ENCODED:
         return req.to_urlencoded()
     elif content_type == JSON_ENCODED:
@@ -65,96 +57,3 @@ def get_http_body(req, content_type):
         raise UnSupported(
             "Unsupported content type: '%s'" % content_type)
 
-
-def get_or_post(uri, method, req, content_type=DEFAULT_POST_CONTENT_TYPE,
-                accept=None, **kwargs):
-    """
-    Create the information pieces necessary for sending a request.
-    Depending on whether the request is done using GET or POST the request
-    is placed in different places and serialized into different formats.
-
-    :param uri: The URL pointing to where the request should be sent
-    :param method: Which method that should be used to send the request
-    :param req: The request as a :py:class:`oidcmsg.message.Message` instance
-    :param content_type: Which content type to use for the body
-    :param accept: Whether an Accept header should be added to the HTTP request
-    :param kwargs: Extra keyword arguments.
-    :return:
-    """
-    resp = {}
-    if method in ["GET", "DELETE"]:
-        if req.keys():
-            _req = req.copy()
-            comp = urlsplit(str(uri))
-            if comp.query:
-                _req.update(parse_qs(comp.query))
-
-            _query = str(_req.to_urlencoded())
-            resp['url'] = urlunsplit((comp.scheme, comp.netloc, comp.path,
-                                      _query, comp.fragment))
-        else:
-            resp['url'] = uri
-    elif method in ["POST", "PUT"]:
-        resp['url'] = uri
-        if content_type == URL_ENCODED:
-            resp['body'] = req.to_urlencoded()
-        elif content_type == JSON_ENCODED:
-            resp['body'] = req.to_json()
-        else:
-            raise UnSupported(
-                "Unsupported content type: '%s'" % content_type)
-
-        header_ext = {"Content-Type": content_type}
-        if accept:
-            header_ext = {"Accept": accept}
-
-        if "headers" in kwargs.keys():
-            kwargs["headers"].update(header_ext)
-        else:
-            kwargs["headers"] = header_ext
-        resp['kwargs'] = kwargs
-    else:
-        raise UnSupported("Unsupported HTTP method: '%s'" % method)
-
-    return resp
-
-
-def match_to_(val, vlist):
-    if isinstance(vlist, str):
-        if vlist.startswith(val):
-            return True
-    else:
-        for v in vlist:
-            if v.startswith(val):
-                return True
-    return False
-
-
-SORT_ORDER = {'RS': 0, 'ES': 1, 'HS': 2, 'PS': 3, 'no': 4}
-
-
-def sort_sign_alg(alg1, alg2):
-    if SORT_ORDER[alg1[0:2]] < SORT_ORDER[alg2[0:2]]:
-        return -1
-    elif SORT_ORDER[alg1[0:2]] > SORT_ORDER[alg2[0:2]]:
-        return 1
-    else:
-        if alg1 < alg2:
-            return -1
-        elif alg1 > alg2:
-            return 1
-        else:
-            return 0
-
-
-def add_path(url, path):
-    if url.endswith('/'):
-        if path.startswith('/'):
-            return '{}{}'.format(url, path[1:])
-        else:
-            return '{}{}'.format(url, path)
-    else:
-        if path.startswith('/'):
-            return '{}{}'.format(url, path)
-        else:
-            return '{}/{}'.format(url, path)
--- tests/test_03_util.py
+++ tests/test_03_util.py
@@ -1,96 +1,36 @@
 import json
-import pytest
-
 from urllib.parse import parse_qs
-from urllib.parse import urlparse
+from urllib.parse import urlsplit
+
+from oidcmsg.oauth2 import AccessTokenRequest
+from oidcmsg.oauth2 import AuthorizationRequest
 
 from oidcservice import util
 from oidcservice.util import JSON_ENCODED
+from oidcservice.util import URL_ENCODED
 
-from oidcmsg.exception import UnSupported
-from oidcmsg.oidc import AuthorizationRequest
-from oidcmsg.oidc import AccessTokenRequest
-
-__author__ = 'DIRG'
+__author__ = 'Roland Hedberg'
 
 
-def query_string_compare(query_str1, query_str2):
-    return parse_qs(query_str1) == parse_qs(query_str2)
-
-
-def url_compare(url1, url2):
-    url1 = urlparse(url1)
-    url2 = urlparse(url2)
-
-    if url1.scheme != url2.scheme:
-        return False
-    if url1.netloc != url2.netloc:
-        return False
-    if url1.path != url2.path:
-        return False
-    if not query_string_compare(url1.query, url2.query):
-        return False
-    if not query_string_compare(url1.fragment, url2.fragment):
-        return False
-
-    return True
-
-
-def test_get():
-    uri = u'https://localhost:8092/authorization'
+def test_get_http_url():
+    url = u'https://localhost:8092/authorization'
     method = 'GET'
-    values = {'state': 'urn:uuid:92d81fb3-72e8-4e6c-9173-c360b782148a',
+    values = {'acr_values': 'PASSWORD',
+              'state': 'urn:uuid:92d81fb3-72e8-4e6c-9173-c360b782148a',
               'redirect_uri':
                   'https://localhost:8666/919D3F697FDAAF138124B83E09ECB0B7',
-              'response_type': 'code',
-              'client_id': u'ok8tx7ulVlNV',
+              'response_type': 'code', 'client_id': 'ok8tx7ulVlNV',
               'scope': 'openid profile email address phone'}
     request = AuthorizationRequest(**values)
 
-    resp = util.get_or_post(uri, method, request)
-
-    assert set(resp.keys()) == {'url'}
-    assert url_compare(resp['url'],
-                       u"https://localhost:8092/authorization?state=urn%3A"
-                       "uuid%3A92d81fb3-72e8-4e6c-9173-c360b782148a&"
-                       "redirect_uri=https%3A%2F%2Flocalhost%3A8666"
-                       "%2F919D3F697FDAAF138124B83E09ECB0B7&"
-                       "response_type=code&client_id=ok8tx7ulVlNV&scope"
-                       "=openid+profile+email+address+phone")
+    _url = util.get_http_url(url, request, method)
+    _part = urlsplit(_url)
+    _req = parse_qs(_part.query)
+    assert set(_req.keys()) == {'acr_values', 'state', 'redirect_uri',
+                                'response_type', 'client_id', 'scope'}
 
 
-def test_post():
-    method = 'POST'
-    uri = u'https://localhost:8092/token'
-    values = {
-        'redirect_uri':
-            'https://localhost:8666/919D3F697FDAAF138124B83E09ECB0B7',
-        'code': 'Je1iKfPN1vCiN7L43GiXAuAWGAnm0mzA7QIjl',
-        'grant_type': 'authorization_code'}
-    request = AccessTokenRequest(**values)
-    kwargs = {'scope': '',
-              'state': 'urn:uuid:92d81fb3-72e8-4e6c-9173-c360b782148a',
-              'authn_method': 'client_secret_basic', 'key': [],
-              'headers': {'Authorization': 'Basic aGVqOmhvcHA='}}
-
-    resp = util.get_or_post(uri, method, request, content_type=JSON_ENCODED,
-                            **kwargs)
-    assert set(resp.keys()) == {'url', 'body', 'kwargs'}
-
-    assert resp['url'] == u'https://localhost:8092/token'
-    assert json.loads(resp['body']) == request.to_dict()
-
-    assert resp['kwargs'] == {
-        'scope': '',
-        'state':
-            'urn:uuid:92d81fb3-72e8-4e6c-9173-c360b782148a',
-        'authn_method': 'client_secret_basic', 'key': [],
-        'headers': {'Content-Type':'application/json',
-                    'Authorization': 'Basic aGVqOmhvcHA='}}
-
-
-def test_unsupported():
-    uri = u'https://localhost:8092/token'
+def test_get_http_body_default_encoding():
     values = {
         'redirect_uri':
             'https://localhost:8666/919D3F697FDAAF138124B83E09ECB0B7',
@@ -99,29 +39,59 @@ def test_unsupported():
                 '2t+gwuoRoapEXJyY2wrvg9cWTW2vxsZU+SuWzZlMDXc=',
         'grant_type': 'authorization_code'}
     request = AccessTokenRequest(**values)
-    kwargs = {'scope': '',
+
+    body = util.get_http_body(request)
+
+    _req = parse_qs(body)
+    assert set(_req.keys()) == {'code', 'grant_type', 'redirect_uri'}
+
+
+def test_get_http_body_url_encoding():
+    values = {
+        'redirect_uri':
+            'https://localhost:8666/919D3F697FDAAF138124B83E09ECB0B7',
+        'code': 'Je1iKfPN1vCiN7L43GiXAuAWGAnm0mzA7QIjl'
+                '/YLBBZDB9wefNExQlLDUIIDM2rT'
+                '2t+gwuoRoapEXJyY2wrvg9cWTW2vxsZU+SuWzZlMDXc=',
+        'grant_type': 'authorization_code'}
+    request = AccessTokenRequest(**values)
+
+    body = util.get_http_body(request, URL_ENCODED)
+
+    _req = parse_qs(body)
+    assert set(_req.keys()) == {'code', 'grant_type', 'redirect_uri'}
+
+
+def test_get_http_body_json():
+    values = {
+        'redirect_uri':
+            'https://localhost:8666/919D3F697FDAAF138124B83E09ECB0B7',
+        'code': 'Je1iKfPN1vCiN7L43GiXAuAWGAnm0mzA7QIjl'
+                '/YLBBZDB9wefNExQlLDUIIDM2rT'
+                '2t+gwuoRoapEXJyY2wrvg9cWTW2vxsZU+SuWzZlMDXc=',
+        'grant_type': 'authorization_code'}
+    request = AccessTokenRequest(**values)
+
+    body = util.get_http_body(request, JSON_ENCODED)
+
+    _req = json.loads(body)
+    assert set(_req.keys()) == {'code', 'grant_type', 'redirect_uri'}
+
+
+def test_get_http_url_with_qp():
+    url = u'https://localhost:8092/authorization?test=testslice'
+    method = 'GET'
+    values = {'acr_values': 'PASSWORD',
               'state': 'urn:uuid:92d81fb3-72e8-4e6c-9173-c360b782148a',
-              'authn_method': 'client_secret_basic', 'key': [],
-              'headers': {
-                  'Authorization': 'Basic '
-                                   'b2s4dHg3dWxWbE5WOjdlNzUyZDU1MTc0NzA0NzQzYjZiZWJk'
-                                   'YjU4ZjU5YWU3MmFlMGM5NDM4YTY1ZmU0N2IxMDA3OTM1'}
-              }
-    method = 'UNSUPPORTED'
-    with pytest.raises(UnSupported):
-        util.get_or_post(uri, method, request, **kwargs)
+              'redirect_uri':
+                  'https://localhost:8666/919D3F697FDAAF138124B83E09ECB0B7',
+              'response_type': 'code', 'client_id': 'ok8tx7ulVlNV',
+              'scope': 'openid profile email address phone'}
+    request = AuthorizationRequest(**values)
 
-
-def test_match_to():
-    str0 = "abc"
-    str1 = "123"
-    str3 = "a1b2c3"
-
-    test_string = "{}{}{}".format(str0, str1, str3)
-    assert util.match_to_(str0, test_string)
-    assert not util.match_to_(str3, test_string)
-
-    list_of_str = ["test_0", test_string, "test_1", str1]
-    assert util.match_to_(str0, list_of_str)
-    assert util.match_to_(str1, list_of_str)
-    assert not util.match_to_(str3, list_of_str)
+    _url = util.get_http_url(url, request, method)
+    _part = urlsplit(_url)
+    _req = parse_qs(_part.query)
+    assert set(_req.keys()) == {'acr_values', 'state', 'redirect_uri',
+                                'response_type', 'client_id', 'scope',
+                                'test'}
