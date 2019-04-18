6fa03851c88d689f5b5981693407ed4a81e2ae91
roland <roland@catalogix.se>
2019-04-18 09:53:16 +0200

    Replaced build_services with init_services. New format for service descriptions.

--- src/oidcservice/oauth2/__init__.py
+++ src/oidcservice/oauth2/__init__.py
@@ -1,35 +1,16 @@
 #
-# import inspect
-# import sys
-# from glob import glob
-# from os.path import basename, dirname, join
-#
-# from oidcservice.service import Service
 
-DEFAULT_SERVICES = [
-    ('Authorization', {}),
-    ['AccessToken', {}],
-    ('RefreshAccessToken', {}),
-    ('ProviderInfoDiscovery', {})
-]
-
-
-# def factory(req_name, **kwargs):
-#     pwd = dirname(__file__)
-#     if pwd not in sys.path:
-#         sys.path.insert(0, pwd)
-#     for x in glob(join(pwd, '*.py')):
-#         _mod = basename(x)[:-3]
-#         if not _mod.startswith('__'):
-#             # _mod = basename(x)[:-3]
-#             if _mod not in sys.modules:
-#                 __import__(_mod, globals(), locals())
-#
-#             for name, obj in inspect.getmembers(sys.modules[_mod]):
-#                 if inspect.isclass(obj) and issubclass(obj, Service):
-#                     try:
-#                         if obj.__name__ == req_name:
-#                             return obj(**kwargs)
-#                     except AttributeError:
-#                         pass
-#
+DEFAULT_SERVICES = {
+    "discovery": {
+        'class': 'oidcservice.oauth2.provider_info_discovery.ProviderInfoDiscovery'
+    },
+    'authorization': {
+        'class': 'oidcservice.oauth2.authorization.Authorization'
+    },
+    'access_token': {
+        'class': 'oidcservice.oauth2.access_token.AccessToken'
+    },
+    'refresh_access_token': {
+        'class': 'oidcservice.oauth2.refresh_access_token.RefreshAccessToken'
+    }
+}
